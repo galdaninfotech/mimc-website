b76b34a499ec87e8d5687b20702f81d9fc561f44
Galdan Infotech <galdaninfotech@gmail.com>
2024-12-05 17:28:39 +0530

    Added Timeline page

---
+++ src/routes/about/mimc-timeline/index.tsx
@@ -0,0 +1,228 @@
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { type DocumentHead } from '@builder.io/qwik-city';
+import Sidebar1 from '~/components/sidebar1/sidebar1';
+import styles from './index.css?inline';
+
+export default component$(() => {
+    useStylesScoped$(styles);
+  return (
+    <div class="content" style="min-height: 150px;">
+        <div class="content_inner">
+            <div class="title" style="min-height: 150px;">
+                <div class="title_holder" style="opacity: 1;">
+                    <div class="container">
+                        <div class="container_inner clearfix" style="padding: 0;">
+                            <h1 style="padding: 0; margin: 0;">Timeline of MIMC</h1>
+                            <div class="breadcrumb">
+                                <div class="breadcrumbs"><a href="https://mahabodhi=ladakh.com/">Home</a><span class="delimiter">&gt;</span><span class="current">Timeline of MIMC</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="container_inner clearfix">
+                    <div class="two_columns_75_25 background_color_sidebar grid2 clearfix">
+                        <div class="column1">
+                            <div class="column_inner">
+                                <section class="wpb-content-wrapper">
+                                    <div class="vc_row wpb_row " style="text-align:left">
+                                        <div class="wpb_column vc_column_container vc_col-sm-12">
+                                            <div class="vc_column-inner">
+                                                <div class="wpb_wrapper">
+                                                    <div class="wpb_text_column wpb_content_element ">
+                                                        <div class="wpb_wrapper" style="color: #6c6c6c;">
+
+                                                            <ul class="timeline">
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1986</p>
+                                                                        <h4>Founded MIMC</h4>
+                                                                        <p>
+                                                                            Venerable Bhikkhu Sanghasena founded the Mahabodhi International Meditation Centre (MIMC) in Leh, Ladakh.
+                                                                        </p>
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1991</p>
+                                                                        <h4>Established the Mahabodhi Devachan campus</h4>
+                                                                        <p>
+                                                                            Established the Mahabodhi Devachan campus on 200 acres of barren and isolated land in Choglamsar, approximately 10 kilometres away from Leh town.
+                                                                        </p>
+
+                                                                        <h4>Mahabodhi Girls Hostel</h4>
+                                                                        <p>
+                                                                            Established the Mahabodhi Girls Hostel, a pioneering project providing holistic education and a safe home environment for under-privileged girls from remote homesteads scattered across Ladakh.
+                                                                        </p>
+
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1992</p>
+                                                                        <h3>Mahabodhi Residential School</h3>
+                                                                        <p>
+                                                                            Established Mahabodhi Residential School, dedicated to providing high quality holistic education aimed at eradicating illiteracy and poverty across the region. This school is now providing High School level education to some 600 students.
+                                                                        </p>
+
+                                                                        <h3>Mahabodhi Meditation Centre</h3>
+                                                                        <p>
+                                                                            Established the Mahabodhi Meditation Centre, dedicated to promoting physical and mental well-being, social harmony and world peace.
+                                                                        </p>
+
+                                                                        <h3>Established the Mahabodhi Charitable Hospital </h3>
+                                                                        <p>
+                                                                            Established the Mahabodhi Charitable Hospital - the first non-governmental hospital in Ladakh - dedicated to providing healthcare and support for under-privileged patients through a purpose-built and well-equipped 50 bed hospital.
+                                                                        </p>
+
+                                                                        <h3>Mahabodhi Green Project</h3>
+                                                                        <p>
+                                                                            Started the Mahabodhi Green Project, under which tens of thousands of trees have been planted on the once barren and isolated land at Mahabodhi Devachan.
+                                                                        </p>
+
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1993</p>
+                                                                        <h3>Mahabodhi Mobile Health Clinic</h3>
+                                                                        <p>
+                                                                            Established the Mahabodhi Mobile Health Clinic, dedicated to providing regular healthcare and dental visits for communities in remote and previously inaccessible areas.
+                                                                        </p>
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1995</p>
+                                                                        <h3>International Sakyadhita Conference</h3>
+                                                                        <p>
+                                                                            Hosted the International Sakyadhita Conference on Buddhist Women in Ladakh. Gyalmo Deskit (Her Highness the Queen of Ladakh) and His Holiness the Drikung Kyabgön Chetsang Rinpoche (Supreme Head of the Drikung Kagyu Lineage of Tibetan Buddhism) were the principal guests. Many eminent Buddhist women, nuns, teachers, leaders and thinkers of different Buddhist traditions and sects from all over the world participated in this historic gathering. This conference was of great significance in working toward improving the education, life standards and status, and general recognition of the Bhikkhuni ordination of nuns and women in Ladakh.
+                                                                        </p>
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1996</p>
+                                                                        <h3>Grand Sangha Dana</h3>
+                                                                        <p>
+                                                                            With the generous support of friends and well-wishers from Taiwan, organised a grand Sangha Dana - offering the monastic requisites to some 6,000 monks and nuns from across Ladakh. This offering was the first of its kind in Ladakh. His Holiness the Sakya Trizin Rinpoche (Supreme Head of the Sakya Lineage of Tibetan Buddhism), all the Supreme Heads of the different Buddhist sects of Ladakh, the President of the Ladakh Buddhist Association, the President of the Ladakh Monastery (Gompa) Association, and the Chief Executive Councillor of the Ladakh Autonomous Hill Development Council (LAHDC) were present, as well as many other local and international dignitaries.
+                                                                        </p>
+
+                                                                        <h3>Mahabodhi Boys Hostel</h3>
+                                                                        <p>
+                                                                            Established the ⦁	Mahabodhi Boys Hostel, dedicated to providing holistic education and a safe home environment for under-privileged boys from remote homesteads scattered across Ladakh. At present, there are some 150 boys living in this hostel.
+                                                                        </p>
+
+                                                                        <h3>Mahabodhi New Delhi Branch.</h3>
+                                                                        <p>
+                                                                            Established the Mahabodhi New Delhi Branch.
+                                                                        </p>
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1997</p>
+                                                                        <h3>Mahabodhi Nunnery</h3>
+                                                                        <p>
+                                                                            Established the Mahabodhi Nunnery, a truly pioneering project dedicated to providing a fully integrated and holistic education for young nuns.
+                                                                        </p>
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1988</p>
+                                                                        <h3>International conference for young adults</h3>
+                                                                        <p>
+                                                                            In collaboration with the International Association for Religious Freedom (IARF), based in the UK, organized a 3-day international conference for young adults of different faiths to promote inter-religious harmony, peaceful co-existence and brotherhood. Many delegates, drawn from different parts of India and overseas, participated in this important conference. All the religious heads of the various denominations of Ladakh attended and participated in the event. Dr. Karan Singh (Chairman of the Foreign Affairs Department and India's Ambassador to UNESCO) was the chief guest. The participants visited various religious places (a Buddhist temple, a mosque, a church and a Gurudwara) in Ladakh, with the intention of building greater understanding and mutual trust between the participants.
+                                                                        </p>
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">1999</p>
+                                                                        <h3>Mahabodhi Branch School</h3>
+                                                                        <p>
+                                                                            Founded the ⦁	Mahabodhi Branch School in ⦁	Tingmosgang village, dedicated to providing high-quality holistic education to students in this remote rural location, where at present some 150 students attend each day.
+                                                                        </p>
+                                                                    </div>
+                                                                </li>
+
+                                                                <li class="timeline-event">
+                                                                    <label class="timeline-event-icon"></label>
+                                                                    <div class="timeline-event-copy">
+                                                                        <p class="timeline-event-thumbnail">2001</p>
+                                                                        <h3>MRS Students Pursues Higher Studies</h3>
+                                                                        <p>
+                                                                            Scores of Mahabodhi Residential School graduates have travelled to Bangalore, South India to pursue their higher education studies with the ongoing encouragement and support of the Mahabodhi International Meditation Centre.
+                                                                        </p>
+                                                                    </div>
+                                                                </li>
+                                                                                                                                
+                                                                </ul>
+
+                                                            
+                                                            
+
+                                                   
+
+
+                                                           
+
+
+
+
+
+                                       
+                                                        </div>
+                                                    </div>
+                                                    <div class="separator transparent" style="margin-top:20px;margin-bottom:20px;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                            </div>
+                        </div>
+                        <div class="column2">
+                            <div class="column_inner">
+                                <aside>
+                                    <Sidebar1 />
+                                </aside>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Timeline of MIMC',
+  meta: [
+    {
+      name: 'description',
+      content: 'Timeline of MIMC',
+    },
+  ],
+};
